594beb2559972322025b1a78968bca29ce96ce4e
Pramod Agrawal <pramod.mathura94@gmail.com>
2026-04-14 07:01:47 +0530

    roles

--- lambda/lambda_function_parquet_conv.py
+++ lambda/lambda_function_parquet_conv.py
@@ -29,3 +29,46 @@ def lambda_handler(event, context):
             Key=output_key,
             Body=buffer.getvalue()
         )
+    raw_key = f"raw/source=iata/dt={date.today()}/sales.zip"
+    filename = raw_key.split("/")[-1]
+    timestamp = datetime.now()
+    archive_key = f"archive/{timestamp}_{filename}"    
+    
+    try:
+        # archive original file
+        archived = archive_s3_object(BUCKET, raw_key, archive_key)
+
+        if not archived:
+            raise Exception("Archive failed")
+            
+        # Delete original file
+        delete_s3_object(BUCKET, raw_key)
+
+    except Exception as e:
+        print(f"Process failed: {str(e)}")
+        
+def archive_s3_object(BUCKET, raw_key, archive_key):
+
+    try:
+        s3.copy_object(
+            Bucket=BUCKET,
+            CopySource={"Bucket": BUCKET, "Key": raw_key},
+            Key=archive_key
+        )
+        print(f"Successfully archived: s3://{BUCKET}/{archive_key}")
+        return True
+    except Exception as e:
+        print(f"Unexpected error: {str(e)}")
+        return False
+    
+def delete_s3_object(bucket, raw_key):
+
+    try:
+        s3.delete_object(
+            Bucket=bucket,
+            Key=raw_key
+        )
+        print(f"Successfully deleted: s3://{bucket}/{raw_key}")
+
+    except Exception as e:
+        print(f"Unexpected error: {str(e)}")
